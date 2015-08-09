26e21f6090eb32c73c766f6277ea7ee60fd9613b
electrototo <cristobal@liendo.net>
2015-08-09 15:51:28 -0500

    Added a little feedback status

--- xbeeAPITransmit.ino
+++ xbeeAPITransmit.ino
@@ -14,6 +14,11 @@ SoftwareSerial xbee(2, 3);
 
 Adafruit_AM2315 tempsensor;
 
+float temp, humidity;
+byte discardByte, msb, lsb, ftype;
+
+int frameID, DStatus;
+
 void setup() {
   Serial.begin(9600);
   xbee.begin(9600);
@@ -47,68 +52,118 @@ byte p_length(int hsize, String payload){
   }
 }
 
-void transmit(String payload){
-  int hlen = 14;
-  byte fpacket[p_length(14,payload)];
-  int ifpacket = 0;
-  
-  xbee.write(SDL);
-  xbee.write((byte)MSB);
-  xbee.write(p_length(14, payload));
-
-  
-  xbee.write(FTYPE);
-  fpacket[ifpacket] = FTYPE;
-  ifpacket += 1;
-  
-  xbee.write(0x01); //Frame ID, gotta change this later
-  
-  fpacket[ifpacket] = 0x01;
-  ifpacket += 1;
- 
-  for(int i = 0; i < 8; i++){
-    xbee.write((byte)0x00);
-    fpacket[ifpacket] = 0x00;
-    ifpacket += 1;
+void write_packet(byte packet[], int p_size){
+  for(int i = 0; i < p_size; i++){
+    xbee.write(packet[i]);
   }
-
-  xbee.write(0xFF);
-  
-  fpacket[ifpacket] = 0xFF;
-  ifpacket += 1;
-  
-  xbee.write(0xFE);
-
-  fpacket[ifpacket] = 0xFE;
-  ifpacket += 1;
-
-  xbee.write((byte)RADIUS);
-  
-  fpacket[ifpacket] = RADIUS;
-  ifpacket += 1;
-  
-  xbee.write((byte)OPTIONS);
-
-  fpacket[ifpacket] = OPTIONS;
-  ifpacket += 1;
-
-  for(int i = 0; i < payload.length(); i++){
-    xbee.write((char)payload[i]);
-
-    fpacket[ifpacket] = (char)payload[i];
-    ifpacket += 1;
-  }
-
-  for(int i = 0; i<sizeof(fpacket); i++){
-    Serial.print(fpacket[i], HEX);
-  }
-  Serial.println();
-
-  xbee.write(checksum(fpacket, sizeof(fpacket)));
 }
 
-float temp, humidity;
+void transmit(String payload, byte frame_type=FTYPE){
+  xbee.write(SDL);
 
+  switch(frame_type){
+    case 0x10:                                        //Transmit 
+    {
+      int hlen = 14;
+      byte fpacket[p_length(14,payload)];
+      int ifpacket = 0;
+
+      xbee.write((byte)MSB);
+      xbee.write(p_length(14, payload));
+    
+      fpacket[ifpacket] = FTYPE;
+      ifpacket += 1;
+      
+      fpacket[ifpacket] = 0x01; //Frame ID, gotta change this later
+      ifpacket += 1;
+     
+      for(int i = 0; i < 8; i++){
+        fpacket[ifpacket] = 0x00;
+        ifpacket += 1;
+      }
+    
+      fpacket[ifpacket] = 0xFF;
+      ifpacket += 1;
+    
+      fpacket[ifpacket] = 0xFE;
+      ifpacket += 1;
+      
+      fpacket[ifpacket] = RADIUS;
+      ifpacket += 1;
+    
+      fpacket[ifpacket] = OPTIONS;
+      ifpacket += 1;
+    
+      for(int i = 0; i < payload.length(); i++){
+        fpacket[ifpacket] = (char)payload[i];
+        ifpacket += 1;
+      }
+
+      write_packet(fpacket, sizeof(fpacket));
+    
+      xbee.write(checksum(fpacket, sizeof(fpacket)));
+    }
+      break;
+      
+    default:
+      Serial.println("No packte was transmitted");
+  }
+  
+}
+
+String transmit_status_read(){
+  // Returns in an array frame ID and delivery status
+  String answer;
+
+  while(xbee.available()){
+    if(xbee.read() == 0x7E){
+      discardByte = xbee.read();
+      
+      byte lsb = xbee.read();
+      byte ftype = xbee.read();
+      byte checksum;
+
+      if(ftype == 0x8B){
+        byte frameID;
+        byte DStatus;
+        
+        for(int i = 0; i < lsb-1; i++){
+          switch(i){
+            case 0:
+              frameID = xbee.read();
+              break;
+            case 4:
+              DStatus = xbee.read();
+              break;
+            default:
+              xbee.read();
+          }
+        }
+        
+        answer = "Frame ID: ";
+        answer.concat(frameID);
+        answer.concat(", Delivery Status: ");
+
+        switch(DStatus){
+          case 0x00:
+            answer.concat("Success!");
+            break;
+          default:
+            answer.concat("Unknown error (gotta change this later)");
+        }
+        
+      } else {
+        for(int i = 0; i < lsb-1; i++){
+          Serial.print(xbee.read());
+        }
+      }
+      
+      checksum = xbee.read();
+    }
+  }
+
+  return answer;
+}
 
 void loop() {
   String payload = "{0:";
@@ -122,7 +177,12 @@ void loop() {
   payload.concat("}");
   
   transmit(payload);
-  delay(500);
+
+  delay(100);
+
+  Serial.println(transmit_status_read());
+
+  delay(4900);
 }
 
 
