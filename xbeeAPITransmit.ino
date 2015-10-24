da9921f2fe96b7d27742e69dcfdae3514a26e2a9
electrototo <cristobal@liendo.net>
2015-10-24 09:25:27 -0500

    Fixed packet received

--- xbeeAPITransmit.ino
+++ xbeeAPITransmit.ino
@@ -124,19 +124,21 @@ String status_read(){
   //Gotta map bytes to failures
   
   String answer;
-  
   byte checksum;
+
+  byte lsb, ftype;
+  byte frameID, source16[2], source64[8];
+  byte rOptions;
 
   while(xbee.available()){
     if(xbee.read() == 0x7E){
       discardByte = xbee.read();
       
-      byte lsb = xbee.read();
-      byte ftype = xbee.read();
+      lsb = xbee.read();
+      ftype = xbee.read();
 
       if(ftype == 0x8B){
-        byte frameID = xbee.read();
-        byte source16[2];
+        frameID = xbee.read();
 
         for(int i = 0; i < 2; i++){
           source16[i] = xbee.read();
@@ -152,6 +154,30 @@ String status_read(){
         else {
           answer = "success";
         }
+      } 
+      else if (ftype == 0x90){            //Zigbee Receive 
+        byte plength = lsb-12;          //Payload length
+        byte pText[plength]; 
+
+        for(int i = 0; i < 8; i++){       //Obtain 64-bit address of sender
+          source64[i] = xbee.read();
+        }
+
+        for(int i = 0; i < 2; i++){       //Obtain 16-bit addres of sender
+          source16[i] = xbee.read();
+        }
+        
+        rOptions = xbee.read();
+
+        for(int i = 0; i < (lsb - 14); i++){
+          pText[i] = xbee.read();
+          Serial.print(pText[i]);
+        }
+
+        xbee.read();                      //Checksum
+
+        answer = "transmit";
+        
       } else {
         for(int i = 0; i < lsb-1; i++){
           Serial.print(xbee.read());
